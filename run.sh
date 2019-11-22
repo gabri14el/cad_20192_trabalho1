50271db4793233baac0236ae3ed7bca3e6b3fb4d
Matheus Borges <matob@live.com>
2019-11-22 20:25:17 -0300

    Alteração do run para executar .bin

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@ echo "fazendo para o omp..."
 for i in 1000 5000 10000 15000 18000 20000 30000 32500 35000 40000 43000 45000 50000
 do
     echo "tamanho $i"
-   ./mandelbrot_omp $i ~/log.csv
+   ./mandelbrot_omp.bin $i ~/log.csv
 done
 
 echo "fazendo para o omp_vec..."
 for i in 1000 5000 10000 15000 18000 20000 30000 32500 35000 40000 43000 45000 50000
 do
     echo "tamanho $i"
-   ./mandelbrot_omp_vec $i ~/log.csv
+   ./mandelbrot_omp_vec.bin $i ~/log.csv
 done
 
 
@@ -18,12 +18,12 @@ echo "fazendo para o vec..."
 for i in 1000 5000 10000 15000 18000 20000 30000 32500 35000 40000 43000 45000 50000
 do
     echo "tamanho $i"
-   ./mandelbrot_vec $i ~/log.csv
+   ./mandelbrot_vec.bin $i ~/log.csv
 done
 
 echo "fazendo para o serial"
 for i in 1000 5000 10000 15000 18000 20000 30000 32500 35000 40000 43000 45000 50000
 do
     echo "tamanho $i"
-   ./mandelbrot_serial $i ~/log.csv
+   ./mandelbrot_serial.bin $i ~/log.csv
 done
